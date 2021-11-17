2d99c76aaa3e343b8c2c579d585bafee00c77b03
“leonshimizu” <lmshimizu@gmail.com>
2021-11-16 18:36:16 -0800

    added router to create/new

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <ul>
+      <li v-for="product in products">
+        <p>ID: {{ product.id }}</p>
+        <p>Name: {{ product.name }}</p>
+        <p>Price: {{ product.price }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data: function () {
+      return {
+        message: "All Products!",
+        products: []
+      };
+    },
+    created: function () {
+      this.indexFunction();
+    },
+    methods: {
+      indexFunction: function() {
+        console.log("in the index function");
+        axios
+          .get(`/products`)
+          .then(response => {
+            console.log(response.data);
+            this.products = response.data
+          })
+      }
+    },
+  };
+</script>
---
+++ src/views/ProductsNew.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="create">
+    <form v-on:submit.prevent="submit()">
+      <h1>Add Product</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Name:</label>
+        <input type="text" v-model="newProductParams.name" />
+      </div>
+      <div>
+        <label>Price:</label>
+        <input type="text" v-model="newProductParams.price" />
+      </div>
+      <div>
+        <label>Description:</label>
+        <input type="text" v-model="newProductParams.description" />
+      </div>
+      <div>
+        <label>Supplier ID:</label>
+        <input type="text" v-model="newProductParams.supplier_id" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from 'axios';
+  export default {
+    data: function () {
+      return {
+        newProductParams: {},
+        errors: []
+      };
+    },
+    created: function () {},
+    methods: {
+      submit: function () {
+        console.log("in the create Function")
+        axios
+          .post('/products', this.newProductParams)
+          .then(response => {
+            console.log(response.data);
+            this.$router.push('/products');
+          })
+      }
+    },
+  };
+</script>
